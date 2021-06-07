b1e757b3223708d1d7a5aa1f2ae9d5a94eca85bb
jessica_wang2 <jessica_wang2@dell.com>
2021-06-07 11:47:39 +0800

    feat: [VXP-29168] cluster ut timeout

--- tests/unit/modules/test_dellemc_vxrail_cluster.py
+++ tests/unit/modules/test_dellemc_vxrail_cluster.py
@@ -14,10 +14,23 @@ from vxrail_ansible_utility.api.cluster_expansion_api import ClusterExpansionApi
 from vxrail_ansible_utility.api.cluster_shutdown_api import ClusterShutdownApi
 
 
+class ExpansionResponse():
+    pass
+
+
 class TestDellEmcVxrailCluster(unittest.TestCase):
 
     def setUp(self):
         self.request_id = "request_id"
+        self.expansion_response = ExpansionResponse()
+        self.expansion_response.request_id = self.request_id
+        self.expansion_response.state = 'COMPLETED'
+        self.expansion_response.owner = 'owner1'
+        self.expansion_response.progress = 100
+        self.expansion_response.extension = '{"hosts":[{"name": "hostname"}]}'
+        self.expansion_response.start_time = '123456'
+        self.expansion_response.end_time = '123457'
+        self.expansion_response.id = 123
 
     @mock.patch.object(ClusterExpansionApi, 'v1_cluster_expansion_validate_post')
     @mock.patch.object(ClusterShutdownApi, 'v1_requests_id_get')
@@ -26,19 +39,15 @@ class TestDellEmcVxrailCluster(unittest.TestCase):
         with patch.multiple(basic.AnsibleModule, exit_json=exit_json, fail_json=fail_json):
             v1_cluster_expansion_validate_post.return_value.request_id = self.request_id
             v1_cluster_expansion_post.return_value.request_id = self.request_id
-            v1_requests_id_get.return_value.state = 'FAILED'
             v1_cluster_expansion_post.return_value.state = 'COMPLETED'
-            v1_requests_id_get.return_value.owner = ''
-            v1_requests_id_get.return_value.progress = 100
-            v1_requests_id_get.return_value.extension = '{"hosts":[{"name": "hostname"}]}'
-            v1_requests_id_get.return_value.start_time = '123456'
-            v1_requests_id_get.return_value.end_time = '123457'
+            v1_requests_id_get.side_effect = [self.expansion_response, self.expansion_response]
+            v1_requests_id_get.return_value = self.expansion_response
 
             set_module_args(
                 {
                     "vxmip": "vxmip",
                     "vxm_version": "vxm_version",
-                    "timeout": 10,
+                    "timeout": 50,
                     "vcadmin": "vc_admin",
                     "vcpasswd": "vc_password",
                     "host_psnt": "host_psnt",
@@ -57,10 +66,7 @@ class TestDellEmcVxrailCluster(unittest.TestCase):
             )
 
             with self.assertRaises(AnsibleExitJson) as result:
-                try:
-                    dellemc_vxrail_cluster.main()
-                except AssertionError as e:
-                    assert isinstance(e, AssertionError)
+                dellemc_vxrail_cluster.main()
 
 
 if __name__ == "__main__":
