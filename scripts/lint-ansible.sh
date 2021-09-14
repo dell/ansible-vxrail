206dc07cacfa96eed1d7f1e20e6e1dad6308d5ef
Wei <Jerry.Wei@vce.com>
2021-09-14 16:17:12 +0800

    feat(cicd): [VXDOPS-5735] Setup CICD pipelines

---
+++ scripts/lint-ansible.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Copyright 2021 Dell Inc. or its subsidiaries. All Rights Reserved
+#
+# This software contains the intellectual property of Dell Inc. or is licensed to Dell Inc. from third parties.
+# Use of this software and the intellectual property contained therein is expressly limited to the terms and
+# conditions of the License Agreement under which it is provided by or on behalf of Dell Inc. or its subsidiaries.
+
+set -e
+
+echo ">>>>>>>>>>>> ansible lint"
+
+ansible-lint docs/samples/
